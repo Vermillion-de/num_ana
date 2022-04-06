82746bda215e76e5c4c3e5d188a366b06c5dff4f
VerMe <vermillionde@gmail.com>
2022-04-06 22:08:21 +0800

    OK

--- code6/code6hw.cpp
+++ code6/code6hw.cpp
@@ -3,10 +3,10 @@
 #include <math.h>
 #include <iostream>
 using namespace std;
+double eps(double x){
+    return exp(-x) / 2 * (1 + (2 * sin(2*x) - cos(2*x))/5);
+} 
 int main(){
-
-
-
-
+    
     return 0;
 }
