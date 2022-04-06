1014a52406aa3fb1c3bd4febe14a7f3ba1161be5
VerMe <vermillionde@gmail.com>
2022-04-06 22:19:49 +0800

    ok

--- code6/code6hw.cpp
+++ code6/code6hw.cpp
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
+#define EPS 1e-3
 using namespace std;
 double eps(double x){
-    return exp(-x) / 2 * (1 + (2 * sin(2*x) - cos(2*x))/5);
+    return exp(-x) / 2 * (1 + ( cos(2*x) - 2 * sin(2*x) )/5);
 } 
 int main(){
-    
+    int c = log(1/(5*EPS));
+    while (eps(c) > EPS){
+        c++;
+    }
+    cout << "the condition is, " << c << endl;
+    double r = c, l = c - 1, m = (l+r)/2;
+    for(int i = 0; i < 10; i++){
+        if(eps(m) < EPS){
+            r = m;
+        }
+        else{
+            l = m;
+        }
+        m = (r+l)/2;
+    }
+    cout << "after iter 10times, " << m << endl;
     return 0;
 }
